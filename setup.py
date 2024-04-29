f50d64419d06567d70629432f673fc80e85f31ca
perzibel <dorsegal1994@gmail.com>
2024-04-29 09:01:03 +0300

    version 0.1.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(file_name):
 
 setup(
     name='static-analysis-script',
-    version='0.1.3',
+    version='0.1.4',
     author='Perzibel',
     author_email='perzibel@outlook.com',
     description='A utility to perform static analysis on files.',
@@ -30,7 +30,7 @@ setup(
     ],
     include_package_data=True,
     package_data={
-        "analysis": ["bin/strings.exe"],  # Make sure the path matches your structure
+        "analysis": ["../bin/strings.exe"],  # Make sure the path matches your structure
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
